75aee8b9a21995a9bad5517a2451361fd22b8fe4
Scott Checko <schecko@sfu.ca>
2018-08-14 23:45:13 -0700

    displaying name and github link

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+var GITHUB = "https://github.com/schecko";
+
+function openInNewTab(url) {
+	var win = window.open(url, '_blank');
+	console.log(win)
+	win.focus();
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+	  		<div className="App">
+				<h1> Scott Checko </h1>
+				<a href="{ GITHUB }" onClick = { () => { openInNewTab(GITHUB) } }>{ GITHUB }</a>
+	  		</div>
+		);
+  	}
 }
 
 export default App;
